64e6b6b36741918cc15291002dcffeaedb30376a
Mukesh Murugan <iammukeshm@gmail.com>
2022-12-10 23:43:13 +0530

    spell fix

--- src/building-blocks/Exceptions/ConfigurationNotFoundException.cs
+++ src/building-blocks/Exceptions/ConfigurationNotFoundException.cs
@@ -4,7 +4,7 @@ namespace BuildingBlocks.Exceptions;
 
 public class ConfigurationNotFoundException : CustomException
 {
-    public ConfigurationNotFoundException(string settingName) : base(string.Format("Configurations for {0} Not Found.", settingName), null, HttpStatusCode.InternalServerError)
+    public ConfigurationNotFoundException(string settingName) : base(string.Format("Configuration for {0} Not Found.", settingName), null, HttpStatusCode.InternalServerError)
     {
     }
 }
